adc9b7b4fc3d32cecbd706d59059f8dc475b67a9
Robert DeNicola <thedizzle23@gmail.com>
2012-06-09 12:59:56 -0400

    message

---
+++ TableViewTabs/TableViewTabsTests/TableViewTabsTests.h
@@ -0,0 +1,13 @@
+//
+//  TableViewTabsTests.h
+//  TableViewTabsTests
+//
+//  Created by Robert DeNicola on 6/9/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface TableViewTabsTests : SenTestCase
+
+@end
---
+++ TableViewTabs/TableViewTabsTests/TableViewTabsTests.m
@@ -0,0 +1,32 @@
+//
+//  TableViewTabsTests.m
+//  TableViewTabsTests
+//
+//  Created by Robert DeNicola on 6/9/12.
+//  Copyright (c) 2012 Developmental Applications. All rights reserved.
+//
+
+#import "TableViewTabsTests.h"
+
+@implementation TableViewTabsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in TableViewTabsTests");
+}
+
+@end
